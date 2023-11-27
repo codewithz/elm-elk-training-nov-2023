b92d5fa4dada3a6a1a65ab23df6953ab861c9a6d
Code With Z <71726246+codewithz@users.noreply.github.com>
2023-11-27 14:58:35 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -33,3 +33,18 @@ curl -XPUT 127.0.0.1:9200/_bulk?pretty -HContent-Type:application/json --data-bi
         }
     }
 }
+
+#---------------------- Query for Franchise and Film-----------------------
+
+{
+    "query":{
+        "has_parent":{
+            "parent_type":"franchise",
+            "query":{
+                "match":{
+                    "title":"Star Wars"
+                }
+            }
+        }
+    }
+}
