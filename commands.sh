d6a7803aeb64c9e0c92b9e23547cb1fd0f40cb41
Code With Z <71726246+codewithz@users.noreply.github.com>
2023-11-27 14:09:24 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,22 @@
 curl -XPUT 127.0.0.1:9200/_bulk?pretty -HContent-Type:application/json --data-binary @movies.json
+
+#-------------------------- New Mapping for Movies--------------------------
+
+{
+    "mappings":{
+        "properties":{
+            "id":{
+                "type":"integer"
+            },
+            "year":{
+                "type":"date"
+            },
+             "genre":{
+                "type":"keyword"
+            },
+             "title":{
+                "type":"text","analyzer":"english"
+            }
+        }
+    }
+}
